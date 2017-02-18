9c30eea3bee90362744c8729b91ec07362967640
hsienchin <eehsienchin@gmail.com>
2017-02-19 01:40:54 +0800

    add cnn feature make

--- src/util.py
+++ src/util.py
@@ -3,6 +3,15 @@ import tf_idf
 import random
 import nltk
 from collections import Counter
+def clean_tag(tag_str):
+    tag = []
+    for t in tag_str.split():
+        if '-' in t:
+            for tt in t.split('-'):
+                tag.append(tt)
+        else:
+            tag.append(t)
+    return tag
 
 def load_data(data_dir, data_type):
     # for example:  data_type = '_light.csv'
@@ -37,19 +46,58 @@ def make_cnn_feature(data_light, data_with_stop_words, word_embedding_size=200, 
         print "read word2vec model..."
     except:
         print "no pretrained word2vec model..."
-    
+    feature_size = 7
     all_pos_tag = ['CC','CD','DT','EX','FW','IN','JJ','JJR','JJS','LS','MD','NN','NNS','NNP','NNPS','PDT','POS','PRP','PRP$','RB','RBR','RBS','RP','SYM','TO','UH','VB','VBD','VBG','VBN','VBP','VBZ','WDT','WP','WP$','WRB']
-    word_embedding = []
-    pos_tag_vec = []
-    y_haveTag = []
+    x_word_embedding = []
+    x_pos_tag_seq = []
+    x_tf_idf = []
+    y_hasTag = []
     y_TagPosition = []
-    for title in data_with_stop_words['title']:
-        words = word_tokenize(title)
-        temp = np.zeros((sent_size,word_embedding_size))
-        if len(words) < sent_size:
-            
-            temp.append(
-        
+    title_idf, content_idf = tf_idf.inverse_frequency(data_light, opt='smooth')
+    word2count = word_count(data_light)
+    max_word_count = float(max(word2count.values()))
+    title_content = ['title', 'content']
+    for col in title_content:
+        for index, sen in enumerate(data_with_stop_words[col]):
+            words = word_tokenize(sen)
+            words_light = word_tokenize(data_light[col][index])
+            tags = clean_tag(data_with_stop_word['tags'][index])
+            hasTag = False
+            word_embedding = np.zeros((sent_size, word_embedding_size))
+            word_tf_idf = np.zeros((sent_size, feature_size))
+            pos_tag_seq = [0] * sent_size
+            position = [0] * sent_size
+            if len(words) < sent_size:
+                pos_tags = pos_tag(words)
+                for i in range(0, sent_size):
+                    word_embedding[i] = model[words[i]]
+                    tf = tf_idf.term_frequency(words[i], sen)
+                    word_tf_idf[i][0] = tf                                  #tf
+                    if col == 'title':                                      #idf
+                        word_tf_idf[i][1] = title_idf[words[i]]             
+                    else:
+                        word_tf_idf[i][1] = content_idf[words[i]]
+                    word_tf_idf[i][2] = tf*word_tf_idf[i][1]                #tf-idf
+                    word_tf_idf[i][3] = 1 if col == 'title' else 0          #is title
+                    word_tf_idf[i][4] = 1 if word[i] in words_light else 0  #is not stop word
+                    word_tf_idf[i][5] = word2count[words[i]]/max_word_count #word count
+                    word_tf_idf[i][6] = find_position(word, sen)
+                    if pos_tags[i][1] in all_pos_tag :
+                        pos_tag_seq[i] = all_pos_tag.index(pos_tags[i][1]) + 1
+                x_word_embedding.append(word_embedding)
+                x_tf_idf.append(word_tf_idf)
+                x_pos_tag_seq.append(pos_tag_seq)
+                for tag in tags:
+                    if tag in words:
+                        hasTag = True
+                        position[words.index(tag)] = 1
+                if hasTag:
+                    y_hasTag.append([1,0])
+                else:
+                    y_hasTag.append([0,1])
+                y_TagPosition.append(position)
+    return x_word_embedding, x_tf_idf, x_pos_tag_seq, y_hasTag, y_TagPosition
+    
 
 def make_feature(data_light, data_with_stop_words, negtive_rate=0.9):
     # for example: data_light = dataframe['cooking']
@@ -59,8 +107,8 @@ def make_feature(data_light, data_with_stop_words, negtive_rate=0.9):
     word2count = word_count(data_light)
     max_word_count = float(max(word2count.values()))
     for index, title in enumerate(data_light['title']):
-        tags = data_light['tags'][index].split()
-
+        #tags = data_light['tags'][index]).split()
+        tags = clean_tag(data_light['tags'][index])
         for word in tf_idf.clean_string(title).split():
             tf = tf_idf.term_frequency(word, title)
             feature = [ tf, title_idf[word], tf*title_idf[word], 1, word2count[word]/max_word_count,
